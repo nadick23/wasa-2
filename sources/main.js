5a1a6e6076314757e1779ef3ac4fa9c4a4fd042a
nadick23 <espinozanicky40@gmail.com>
2024-06-07 15:14:51 -0500

    imagen, y fondo de texto

--- sources/main.js
+++ sources/main.js
@@ -1,3 +1,11 @@
-function wasa(){
-    
+function wasa() {
+    alert("contactanos en: niki@gimail")
+
+
 }
+document.addEventListener("DOMContentLoaded", () => {
+    const image = document.getElementById("wasa3")
+    image.addEventListener('click', () => {
+        alert("has tocado al sus")
+    })
+})
