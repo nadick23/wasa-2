b9b28c56eaaffa01fac53f995d51539547daf367
Alejandr0x <ytalejandrovevo@gmail.com>
2024-06-07 15:45:01 -0500

    Version 1.0.2

--- sources/main.js
+++ sources/main.js
@@ -1,11 +1,8 @@
-function wasa() {
+function contacto() {
     alert("contactanos en: niki@gimail")
-
-
 }
-document.addEventListener("DOMContentLoaded", () => {
+
     const image = document.getElementById("wasa3")
     image.addEventListener('click', () => {
         alert("has tocado al sus")
     })
-})
