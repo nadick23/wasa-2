6a533d104cbcf03ac5669eacb63788ae749ab5ef
nadick23 <espinozanicky40@gmail.com>
2024-06-08 15:15:15 -0500

    cambio de imagen

--- sources/main.js
+++ sources/main.js
@@ -2,7 +2,3 @@ function contacto() {
     alert("contactanos en: niki@gimail")
 }
 
-    const image = document.getElementById("wasa3")
-    image.addEventListener('click', () => {
-        alert("has tocado al sus")
-    })
